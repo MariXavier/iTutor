a17d6879068c5a5b965131b42a47584b4284b1e4
MariXavier <marianaxga@hotmail.com>
2019-06-18 15:13:37 -0300

    Criado classe para fazer a validação dos campos antes de enviar o cadastro para o banco de dados.

---
+++ SistemaProjeto_iTutor/Classes/ValidarCampos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaProjeto_iTutor.Classes
+{
+    public static class ValidarCampos
+    {
+        public static bool validarNome(string nome)
+        {
+            return !(string.IsNullOrWhiteSpace(nome)); //previne null, vazio e espaço em branco
+        }
+    }
+}
